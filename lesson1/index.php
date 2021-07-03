d79f80aa09e14fa5b430b2a0da57bf46d92d49ea
Brunevskiy Ivan <brunevskiyivan@gmail.com>
2021-07-03 17:08:43 +0300

    first hometask

--- lesson1/index.php
+++ lesson1/index.php
@@ -1,1 +1,47 @@
+
 <?php
+//ТИПЫ ДАННЫХ
+
+// Тип данных bool
+$bool = true;
+
+// Тип данных integer
+$int = 27;
+
+// Тип данных string
+$str = 'string';
+
+// Тип данных float
+$float = 12.15;
+
+// Тип данных NULL
+$null = NUll;
+
+echo $bool;
+echo '<br>';
+echo $int;
+echo '<br>';
+echo $str;
+echo '<br>';
+echo $float;
+echo '<br>';
+echo $null;
+
+//МАТЕМАТИЧЕСКИЕ ОПЕРАЦИИ
+
+$x = 4;
+$y = 11;
+
+var_dump($x + $y);
+var_dump($y - $x);
+var_dump($x * $y);
+var_dump($y / $x);
+var_dump($x ** $y);
+
+//ОПЕРАЦИИ СРАВНЕНИЯ
+
+var_dump($x > $y);
+var_dump($x < $y);
+var_dump($x >= $y);
+var_dump($y <= $x);
+var_dump($x !== $y);
